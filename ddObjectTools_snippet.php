993eebb4cbcb33f7a4e7d3a583eadc6cdd839c6d
Ilyas Ronef <ilyas@DivanDesign.ru>
2025-11-19 15:19:19 +0400

    * Minor changes (fixed inactive Ronef.ru links).

--- ddObjectTools_snippet.php
+++ ddObjectTools_snippet.php
@@ -7,7 +7,7 @@
  * 
  * @link https://code.divandesign.ru/modx/ddobjecttools
  * 
- * @copyright 2020–2024 Ronef {@link https://Ronef.ru }
+ * @copyright 2020–2024 Ronef {@link https://Ronef.me }
  */
 
 // Include (MODX)EvolutionCMS.libraries.ddTools
